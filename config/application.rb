02befa8ce3da30cac49ff64723190b7fd9a3c4b3
iurevych <sergii.iurevych@railsware.com>
2012-02-16 09:56:09 +0200

    Include HAML

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_mailer/railtie"
 require 'sprockets/railtie'
+require 'haml'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
