ea73ab7f526aa35c26aa375b69837be70d27c2e4
iurevych <sergii.iurevych@railsware.com>
2012-02-17 11:25:57 +0200

    Set Futurico UI as a default homepage

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Scaffy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'static#index'
+  root :to => 'static#futurico'
   match 'static/futurico' => 'static#futurico'
 
   # See how all your routes lay out with "rake routes"
