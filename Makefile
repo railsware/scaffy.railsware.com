f3581f0f9ce8a3614ab07352d926bdb5484cd219
iurevych <sergii.iurevych@railsware.com>
2012-03-06 18:05:59 +0200

    Release

--- Makefile
+++ Makefile
@@ -3,11 +3,10 @@ naKolene:
 	mkdir -p public/html/javascripts
 	mkdir -p public/html/stylesheets
 	cp -r app/assets/images/* public/html/images/
+	cp -r app/assets/javascripts/* public/html/javascripts/
 	sass --update app/assets/stylesheets:public/html/stylesheets
 	sass --update --style compressed app/assets/stylesheets/futurico-dark-bundle.sass:public/html/stylesheets/futurico-dark-bundle.css
 	rm public/html/bundle.zip
 	zip -r public/html/bundle.zip public/html
 	git add .
-	git commit -a -m "Release"
-	git push origin HEAD
-	git push heroku master
+	git commit -m "Release"
---
+++ public/html/javascripts/SCF.ui.js
@@ -0,0 +1,56 @@
+SCF = {};
+
+$(document).ready(function() {
+    // Init custom selects
+    $(".select").chosen();
+
+    // Init slideshow
+    SCF.Slideshow.init();
+
+    // Init appreciate
+    SCF.Appreciate.init();
+
+    // Init equalizer
+    SCF.Equalizer.init();
+
+    // Init datepicker
+    SCF.Datepicker.init();
+
+    // Init scrollbox
+    SCF.Scrollbox.init();
+
+    // Init commutator
+    SCF.Commutator.init();
+
+    // Init tabbox
+    var tabBox = new SCF.Tabbox(".tabbox");
+    tabBox.init();
+    $(".tabbox-stuff").tabs();
+
+    // Init pagination
+    SCF.Pagination.init();
+
+    // Custom radios
+    $(".radiogroup").each(function() {
+        var parent = this;
+
+        $(this).find(".radio").click(function() {
+        $(parent).find(".radio").removeClass("checked");
+            $(this).addClass("checked");
+        });
+    });
+
+    // Custom checkboxes
+    $(".checkbox").click(function() {
+        $(this).toggleClass("checked");
+    });
+
+    $(".checkbox-light").click(function() {
+        $(this).toggleClass("checked");
+    });
+
+    // Init custom placeholder for text inputs
+    $("input[placeholder], textarea[placeholder]").placeholder();
+
+});
+
