dcac93e99c0605f1b6fd3803017cff09f129f1a6
iurevych <sergii.iurevych@railsware.com>
2012-06-08 19:21:16 +0300

    RW badge. CSS refactoring

---
+++ js/ym.js
@@ -0,0 +1,17 @@
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter15049390 = new Ya.Metrika({id:15049390, enableAll: true, webvisor:true});
+        } catch(e) {}
+    });
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
